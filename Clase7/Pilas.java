cda19763b381a655f5163a85c731d6ca2de78776
Kadiaz <131292080+Kadiaz@users.noreply.github.com>
2026-03-18 17:05:33 +0000

    ejemplosPilas

---
+++ Clase7/Pilas.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class Pilas 
+{
+    public static void main(String[] args) 
+    {
+        Stack<Integer> pila = new Stack<>();
+        //Mostrar si la pila está vacía
+        System.out.println(pila.empty()); //true
+        //Agregar elementos a la pila
+        pila.push("Sebastian");
+        pila.push("Alejandro");
+        pila.push("Natalia");
+        pila.push("Juan");
+          //Mostrar el contenido de la pila
+        System.out.println(pila); //[Sebastian, Alejandro, Natalia, Juan]
+        //Mostrar el tope de la pila sin removerlo
+        System.out.println(pila.peek()); // Juan
+        //Mostrar el contenido de la pila
+        System.out.println(pila); //[Sebastian, Alejandro, Natalia, Juan]
+        //Eliminar el elemento que se encuentra en el tope de la pila
+        System.out.println("Se eliminó: " + pila.pop()); //Juan
+        //Mostrar el contenido de la pila
+        System.out.println(pila); //[Sebastian, Alejandro, Natalia]
+    }
+}
