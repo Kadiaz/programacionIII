64ec5ca9d6637e97f7d532a6dff41db2af1fcbc6
Kadiaz <131292080+Kadiaz@users.noreply.github.com>
2026-04-14 21:04:02 +0000

    Cambios de VARIABLES

--- PilasYColas/Verificador.java
+++ PilasYColas/Verificador.java
@@ -1,7 +1,7 @@
 import java.util.Stack;
 import java.util.Scanner;
 
-public class Verificador{
+public class Verificador-{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Ingrese la expresión: ");
